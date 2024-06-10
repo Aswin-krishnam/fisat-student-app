101b15e95203a6d91b5874fe471284bcea5a05cf
Aswin-krishnam <aswinkrishnam16@gmail.com>
2024-06-10 09:53:48 +0530

    Error fix

--- src/component/AddStudent.jsx
+++ src/component/AddStudent.jsx
@@ -4,9 +4,9 @@ import Navbar from './Navbar'
 const AddStudent = () => {
     const [data,setData]=useState(
         {
-            "fname":"",
-            "lname":"",
-            "collage":"",
+            "firstname":"",
+            "lastname":"",
+            "college":"",
             "dob":"",
             "course":"",
             "mobile":"",
@@ -37,19 +37,19 @@ const AddStudent = () => {
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">First name</label>
-                                <input type="text" className="form-control" name='fname' value={data.fname} onChange={inputHandler}/>
+                                <input type="text" className="form-control" name='firstname' value={data.firstname} onChange={inputHandler}/>
 
                             </div>
                             <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
 
                                 <label htmlFor="" className="form-label">Last Name</label>
-                                <input type="text" className="form-control" name='lname' value={data.lname} onChange={inputHandler} />
+                                <input type="text" className="form-control" name='lastname' value={data.lastname} onChange={inputHandler} />
 
                             </div>
                             <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
 
-                                <label htmlFor="" className="form-label">Collage</label>
-                                <input type="text" className="form-control" name='collage' value={data.collage} onChange={inputHandler} />
+                                <label htmlFor="" className="form-label">College</label>
+                                <input type="text" className="form-control" name='college' value={data.college} onChange={inputHandler} />
 
 
                             </div>
